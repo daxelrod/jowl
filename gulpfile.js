e410b175f90ac1069335d11ab0bbb3cb80354cdb
Hutson Betts <hutson@hyper-expanse.net>
2016-02-25 21:39:25 -0600

    chore(gulp): Fix build behavior by returning streams.

--- gulpfile.js
+++ gulpfile.js
@@ -8,33 +8,31 @@ var gulpMocha = require('gulp-mocha');
 var javascriptGlobs = ['*.js', 'src/**/*.js', 'test/**/*.js'];
 
 gulp.task('style', function () {
-  gulp.src(javascriptGlobs)
+  return gulp.src(javascriptGlobs)
     .pipe(jscs())
     .pipe(jscs.reporter())
     .pipe(jscs.reporter('fail'));
 });
 
 gulp.task('lint', function () {
-  gulp.src(javascriptGlobs)
+  return gulp.src(javascriptGlobs)
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter())
     .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('test:unit', function () {
-  gulp.src('test/unit/**/*.js')
+  return gulp.src('test/unit/**/*.js')
     .pipe(gulpMocha());
 });
 
 gulp.task('test:integration', function () {
-  gulp.src('test/integration/**/*.js')
+  return gulp.src('test/integration/**/*.js')
     .pipe(gulpMocha());
 });
 
 gulp.task('test', ['test:unit', 'test:integration']);
 
-gulp.task('build', ['lint', 'style', 'test'], function () {
-  gulp.src('/');
-});
+gulp.task('build', ['lint', 'style', 'test']);
 
 gulp.task('default', ['build']);
