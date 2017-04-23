8af2029a456eb6c29d9bca1f9f8f3becdd6ff7e6
Daniel Axelrod <git@danonline.net>
2017-04-23 03:24:23 +0000

    feat(run): limit p() to taking one argument
    Limit `p()` to taking only one argument, even though `console.json()`
    can handle an unlimited number of arguments.
    
    This is an ergonomic improvement. It is natural to want to pass `p()`
    as an iteratee to lodash, and lodash passes a number of additional
    optional arguments (that users often forget the existence of) to the
    iteratee as well.
    
    Concretely, this is the difference between
    
    ```
    echo '[1,2,3]' | jowl -q 'c.each(function(a){p(a)})'
    ```
    
    and
    
    ```
    echo '[1,2,3]' | jowl -q 'c.each(p)'
    ```
    
    This does mean an extra step is needed to combine keys and values
    when iterating over an object, but the user would need to do that
    anyway in the likely event that they want a format other than
    `value key`.

--- src/lib/run.js
+++ src/lib/run.js
@@ -6,12 +6,10 @@ require('console.json'); // Injects itself directly into the console object
 
 var run = {};
 
-function p() {
-  // console.json can take an unlimited number of arguments
-  console.json.apply(console, arguments); // returns undefined
-
-  // can't return more than one value; the first is least surprising
-  return arguments[0];
+function p(data) {
+  // intentionally limit to just one argument
+  console.json(data); // returns undefined
+  return data;
 }
 
 run.run = function (data, command) {
