210d1b7b8dd8c12296fcdf6881acaa3540a7a89c
Daniel Axelrod <git@danonline.net>
2017-05-14 01:28:07 +0000

    chore(lint): switch to current Airbnb rules
    Prepare to move to es6 by switching from Airbnb ES5 rules to their
    current ruleset. Apply warnings for all rules we will implement; no
    source has changed in this commit.
    
    The trickiest rule to understand was their handling of 'use strict'.
    They set `sourceType` to `module`, which led to complaints about its
    use in script files (and Node will require it to be enabled for const
    and let). Add a second level eslintrc in the lib folder to
    differentiate which files are in fact modules.
    
    They also forbid all 'use strict' because their babel settings add it.
    Reenable it.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-  'extends': 'airbnb-base/legacy',
+  'extends': 'airbnb',
   'env': {
     'node': true
+  },
+  'parserOptions': {
+    'ecmaVersion': 6,
+
+    // Airbnb preset sets this to 'module'
+    // but everything outside of lib is a script
+    'sourceType': 'script'
   },
   'rules': {
     // Previous jscs rules required trailing commas,
@@ -18,5 +25,16 @@ module.exports = {
     // The Airbnb es5 style guide says nothing about using console,
     // and we legitimately need it for output
     'no-console': 'off',
+
+    // Airbnb sets this to never even though their written style guide
+    // never actually mentions it. Babel inserts it for them, but we're
+    // not using Babel.
+    'strict': ['warn', 'safe'], // Warn will be changed to an error after modules are fixed
+
+    // The following settings will be fixed in a future commit
+    'prefer-arrow-callback': 'warn',
+    'no-var': 'warn',
+    'prefer-template': 'warn',
+    'object-shorthand': 'warn',
   }
 };
