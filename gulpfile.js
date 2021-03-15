9cd8df79ee6d269eb89b76c96eeabbbf0209c054
Daniel Axelrod <git@danonline.net>
2021-03-15 00:26:16 +0000

    chore(gulp): move doc lint to package script
    Implement doc linting using pure package.json scripts rather than gulp
    tasks. This is the final step prior to removing gulp completely.
    
    Add a new package.json script `docs`. This runs the markdownlint-cli
    executable, which is a third party CLI wrapper around markdownlint. It
    was chosen over markdownlint-cli2 (by the same author as markdownlint)
    to ensure compatability with Node 6, which we still support. It adds
    more dependencies than I'd like, but will be good enough unless I want
    to implement the recursive search for markdown files myself.
    
    markdownlint-cli unfortunately does not give us direct control over the
    markdownlint version, and necessitates updating markdownlint to a newer
    version that has a new rule that we fail. Therefore, also add
    that rule as disabled. We can revisit the rationalle for each disabled
    rule later and correct if necessary.

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,5 @@
 'use strict';
 
 const gulp = require('gulp');
-const gulpUtil = require('gulp-util');
-const through2 = require('through2');
-const markdownlint = require('markdownlint');
-const fs = require('fs');
 
-const markdownGlobs = ['*.md', 'docs/**/*.md'];
-
-// adapted from DavidAnson/markdownlint README.md@24c33df
-gulp.task('markdownlint', () => (
-  gulp.src(markdownGlobs, { read: false }).pipe(through2.obj((file, enc, next) => {
-    markdownlint(
-      {
-        files: [file.path],
-        config: JSON.parse(fs.readFileSync('./markdownlint.json', 'utf8')),
-      },
-      (err, result) => {
-        let error = err;
-        const resultString = (result || '').toString();
-        if (resultString) {
-          console.log(resultString);
-          error = new gulpUtil.PluginError('markdownlint', {
-            message: 'Markdownlint failed.',
-            showStack: false,
-          });
-        }
-
-        next(error, file);
-      }
-    );
-  }
-  ))
-));
-
-gulp.task('docs', ['markdownlint']);
-
-gulp.task('build', ['docs']);
-
-gulp.task('default', ['build']);
+gulp.task('default', []);
